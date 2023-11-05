65e970b2dad088303c467d7ff702de3e5618dab8
Poterentax <crazy70reg@gmail.com>
2023-11-05 16:07:38 +0700

    new chnges

--- clean svelte/src/App.svelte
+++ clean svelte/src/App.svelte
@@ -1,8 +1,13 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
-  let some: string = 'some'
+  import svelteLogo from "./assets/svelte.svg";
+  import viteLogo from "/vite.svg";
+  import Counter from "./lib/Counter.svelte";
+  let vite: string = "vite";
+  let svelte: string = "svelte";
+  let p2: string = " Click on the Vite and Svelte logos to learn more";
+  let p1: string = "Check out ";
+  let p1_1: string = "SvelteKit";
+  let p1_2: string = "the official Svelte app framework powered by Vite!";
 </script>
 
 <main>
@@ -14,19 +19,21 @@
       <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
     </a>
   </div>
-  <h1>Vite + Svelte</h1>
+  <h1>{vite} + {svelte}</h1>
 
   <div class="card">
     <Counter />
   </div>
 
   <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
+    {p1} <a
+      href="https://github.com/sveltejs/kit#readme"
+      target="_blank"
+      rel="noreferrer">{p1_1}</a
+    >,{p1_2}
   </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+  {p2}
+  <p class="read-the-docs" />
 </main>
 
 <style>
@@ -36,12 +43,15 @@
     will-change: filter;
     transition: filter 300ms;
   }
+
   .logo:hover {
     filter: drop-shadow(0 0 2em #646cffaa);
   }
+
   .logo.svelte:hover {
     filter: drop-shadow(0 0 2em #ff3e00aa);
   }
+
   .read-the-docs {
     color: #888;
   }
